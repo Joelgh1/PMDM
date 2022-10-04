ef95e1fbdd8482e7c4d0b276c848b968cfdcfe7b
Joel Garrido <joelgamar@gmail.com>
2022-10-04 17:11:29 +0200

    Ut01Ex03 views practise with RelativeLayout

---
+++ app/src/main/java/com/joelgh/pmdm_2022/ut01/ex03/presentation/Ut01Ex03bActivity.kt
@@ -0,0 +1,12 @@
+package com.joelgh.pmdm_2022.ut01.ex03.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joelgh.pmdm_2022.R
+
+class Ut01Ex03bActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex03b)
+    }
+}
