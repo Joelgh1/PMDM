5b52c03bad17104f61f5944d48a0baef8e76fa32
Joel Garrido <joelgamar@gmail.com>
2022-10-06 11:35:04 +0200

    Ut01Ex04 trying to replicate the view from "to good to go"

---
+++ app/src/main/java/com/joelgh/pmdm_2022/ut01/ex04/presentation/Ut01Ex04Activity.kt
@@ -0,0 +1,12 @@
+package com.joelgh.pmdm_2022.ut01.ex04.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joelgh.pmdm_2022.R
+
+class Ut01Ex04Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex04)
+    }
+}
